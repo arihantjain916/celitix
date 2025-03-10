f4086a3d97fcead22de79d3c0bc9f650c26c10a0
Arihant Jain <arihantj916@gmail.com>
2025-03-10 10:16:48 +0530

    feat: rename export folder to utils and integrate func in one file

--- src/utils/utills.js
+++ src/utils/utills.js
@@ -1,7 +1,22 @@
+import * as XLSX from "xlsx";
 import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable";
+import toast from "react-hot-toast";
 
+// csv export
+export const exportToExcel = (col, row, name) => {
+  if (row.length === 0) return toast.error("No data found");
+  const data = [col, ...row];
+
+  const worksheet = XLSX.utils.aoa_to_sheet(data);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+  XLSX.writeFile(workbook, `${name}.xlsx`);
+};
+
+// pdf export
 export const exportToPDF = (col, row, name) => {
+  if (row.length === 0) return toast.error("No data found");
   const pdf = new jsPDF({ orientation: "portrait", unit: "px", format: "a4" });
 
   pdf.setFont("Arial");
