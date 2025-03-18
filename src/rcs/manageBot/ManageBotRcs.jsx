0d35f012b091ae5de94c8cf3a105552a7fca0254
Arihant Jain <arihantj916@gmail.com>
2025-03-18 12:33:53 +0530

    feat: fetch all agents data on initial load in manageBotRcs component

--- src/rcs/manageBot/ManageBotRcs.jsx
+++ src/rcs/manageBot/ManageBotRcs.jsx
@@ -1,50 +1,54 @@
-import React, { useState } from 'react'
-import ManageBotTableRcs from './components/ManageBotTableRcs'
-import AnimatedDropdown from '../../whatsapp/components/AnimatedDropdown'
-import UniversalButton from '../../whatsapp/components/UniversalButton';
+import React, { useEffect, useState } from "react";
+import ManageBotTableRcs from "./components/ManageBotTableRcs";
+import AnimatedDropdown from "../../whatsapp/components/AnimatedDropdown";
+import UniversalButton from "../../whatsapp/components/UniversalButton";
 import { IoSearch } from "react-icons/io5";
+import { fetchAllAgents } from "../../apis/rcs/rcs";
 
 const ManageBotRcs = () => {
-    const [selectedOption, setSelectedOption] = useState(null);
-    const [isFetching, setIsFetching] = useState(false);
-    const [filteredData, setFilteredData] = useState([]);
-    const botOptions =[
-      { label: 'Option 1', value: 'option1' },
-      { label: 'Option 2', value: 'option2' },
-    ]
-  
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const botOptions = [
+    { label: "Option 1", value: "option1" },
+    { label: "Option 2", value: "option2" },
+  ];
 
+  //allBotState
+  const [allBots, setAllBots] = useState([]);
 
+  useEffect(() => {
+    async function fetchAllBotsData() {
+      const res = fetchAllAgents();
+      setAllBots(res);
+    }
+
+    fetchAllBotsData();
+  }, []);
 
   return (
-    <div className='w-full'>
-    {/* {isLoading ? (
+    <div className="w-full">
+      {/* {isLoading ? (
       <>
         <Loader />
       </>
     ) : ( */}
       <div>
-        
-        <div className="flex flex-wrap gap-2 items-end mb-2 w-full">
-        <div className="w-full sm:w-56">
+        <div className="flex flex-wrap items-end w-full gap-2 mb-2">
+          <div className="w-full sm:w-56">
             <AnimatedDropdown
-            label="Bot Name"
-            id="botName"
-            name="botName"
-            options={botOptions}
-            value={selectedOption}
-            onChange={(newValue) => setSelectedOption(newValue)}
-            placeholder='select bot name'
+              label="Bot Name"
+              id="botName"
+              name="botName"
+              options={botOptions}
+              value={selectedOption}
+              onChange={(newValue) => setSelectedOption(newValue)}
+              placeholder="select bot name"
             />
           </div>
 
           {/* Search Button */}
           <div className="w-max-content">
-            <UniversalButton
-            
-              label="Show"
-              disabled={isFetching}
-            />
+            <UniversalButton label="Show" disabled={isFetching} />
           </div>
         </div>
 
@@ -56,16 +60,16 @@ const ManageBotRcs = () => {
         ) : (
           <div className="w-full">
             <ManageBotTableRcs
-            id="suggestionreport"
-            name="suggestionreport"
-            data={filteredData}
+              id="suggestionreport"
+              name="suggestionreport"
+              data={allBots}
             />
           </div>
         )}
       </div>
-    {/* )} */}
-  </div>
-  )
-}
+      {/* )} */}
+    </div>
+  );
+};
 
-export default ManageBotRcs
+export default ManageBotRcs;
