d3b1761cbacf23cc14eaf7240c6f1b61c22231c0
Arihant Jain <arihantj916@gmail.com>
2025-03-19 12:30:47 +0530

    feat: add updateUser api in admin api folder

--- src/apis/admin/admin.js
+++ src/apis/admin/admin.js
@@ -6,3 +6,17 @@ export const fetchAllUsers = async (data) => {
     body: JSON.stringify(data),
   });
 };
+
+export const fetchUserbySrno = async (srNo) => {
+  return await fetchWithAuth("/proCpaasRest/user/getuserdetailsById", {
+    method: "POST",
+    body: JSON.stringify({ srNo }),
+  });
+};
+
+export const updateUserbySrno = async (data) => {
+  return await fetchWithAuth("/proCpaasRest/user/updateUserById", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+};
